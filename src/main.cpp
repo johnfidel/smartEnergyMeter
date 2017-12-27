086ea1882b804d1d529776445a12db231d262576
johnfidel <afromann@gmx.ch>
2017-12-27 10:31:30 +0100

    Changed configuration to be able to build the project with gattlib as local dependency

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,35 @@
 #include <stdlib.h>
 #include <algorithm>
 #include <sys/queue.h>
+#include <iostream>
 
 #include "driver/smartEnergyMeter.h"
 #include "app/bt_scan.h"
 #include "gattlib.h"
 
 int main(int argc, const char *argv[])
-{
-	for (int i=0; i < argc; i++)
-	{
-		if (argv[i] == "--scan"){
-			bt_scan(argc, argv);
-		}
-		else
-		{
-			smartEnergyMeter *meter;
-			gatt_connection_t *connection;
+{         
+    std::cout << "hallo" << std::endl;
+    
+    for (int i=0; i < argc; i++)
+    {
+            if (argv[i] == "--scan"){
+                    bt_scan(argc, argv);
+            }
+            else
+            {
+                    smartEnergyMeter *meter;
+                    gatt_connection_t *connection;
 
-			meter = new smartEnergyMeter();
+                    meter = new smartEnergyMeter();
 
-			//gattlib_adapter_open('hci0', );
+                    //gattlib_adapter_open('hci0', );
 
-			//meter->getCurrentValues();
+                    //meter->getCurrentValues();
 
-			free(meter);
-		}
-	}
+                    free(meter);
+            }
+    }
 
-	return 0;
+    return 0;
 }
